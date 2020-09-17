cf14e782d5378922b9acd9c68ac780cf1ef26275
gchuinard <gautier.chuinard@lilo.org>
2020-09-17 11:35:43 +0000

    fix task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,6 +15,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	printf("%d\n", n % 10);
+	printf("Last digit of %d is %d and is %s\n", n, n % 10, (n % 10 == 0) ? "0" :
+			(n % 10 < 6 && n != 0) ? "less than 6 and not 0" :
+			"greater than 5");
 	return (0);
 }
