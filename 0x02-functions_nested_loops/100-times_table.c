dcaffa9b37034f830d693127cf9a01e13ad1faed
gchuinard <gautier.chuinard@lilo.org>
2020-09-22 00:14:32 +0000

    fix task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,6 +1,19 @@
 #include "holberton.h"
 
 /**
+ * print_spaces - Print the nbr of space
+ *
+ * @nbr: the number of space to print
+ *
+ * Return: void
+ */
+void	print_spaces(int nbr)
+{
+	while (nbr-- > 0)
+		_putchar(' ');
+}
+
+/**
  * print_times_table - Print the nbr times tables
  *
  * @nbr_end: the higher table who is print
@@ -16,53 +29,37 @@ void	print_times_table(int nbr_end)
 
 	if (nbr_end >= 0 && nbr_end < 16)
 	{
-		nbr = 0;
-		while (nbr <= nbr_end)
+		nbr = -1;
+		while (nbr++ < nbr_end)
 		{
-			fact = 0;
-			while (fact <= nbr_end)
+			fact = -1;
+			while (fact++ < nbr_end)
 			{
 				result = nbr * fact;
-				if (result < 100 && result > 9 && fact != 0)
-				{
-					_putchar(' ');
-				}
-				else if (result < 10 && fact != 0)
-				{
-					_putchar(' ');
-					_putchar(' ');
-				}
 				if (result > 99)
 				{
+					_putchar(' ');
 					_putchar((result / 100) + '0');
 					_putchar(((result / 10) % 10) + '0');
 					_putchar((result % 10) + '0');
 				}
 				else if (result > 9)
 				{
+					print_spaces(2);
 					_putchar((result / 10) + '0');
 					_putchar((result % 10) + '0');
 				}
 				else
 				{
+					if (fact != 0)
+						print_spaces(3);
 					_putchar((result % 10) + '0');
 				}
 				if (fact < nbr_end)
-				{
 					_putchar(',');
-					_putchar(' ');
-				}
 				else
-				{
 					_putchar('\n');
-				}
-				fact++;
 			}
-			nbr++;
 		}
-	}
-	else
-	{
-		_putchar('\n');
 	}
 }
