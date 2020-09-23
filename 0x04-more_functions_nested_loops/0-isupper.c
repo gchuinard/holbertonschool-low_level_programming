ba98e3f49285deca8ee2882d55103a645849dc2b
gchuinard <gautier.chuinard@lilo.org>
2020-09-23 22:52:40 +0000

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _isupper - Cheeck if c if upper case
+ *
+ * @c: the value of the character to check
+ *
+ * Return:1 (Success) or 0 (Fail)
+ *
+ */
+int	_isupper(int c)
+{
+	if (c > 65 && c < 90)
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int	_putchar(char c);
+int	_isupper(int c);
+
+#endif
