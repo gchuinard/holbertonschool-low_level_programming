934bb7b30e7a7b814fc9ff87a4dec9b146315060
gchuinard <gautier.chuinard@lilo.org>
2020-09-28 18:14:02 +0000

    add task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * _atoi - convert a string to an integer.
+ *
+ * @s: the string to convert.
+ *
+ * Return: nbr
+ *
+ */
+
+
+int     _atoi(char *s)
+{
+	int             nbr;
+	int             sign;
+
+	ret = 0;
+	sign = 0;
+	while ((*s < '0' || *s > '9') && *s != '\0')
+	{
+		s++;
+		if (*s == '-')
+		{
+			sign--;
+		}
+		else if (*s == '+')
+		{
+			sign++;
+		}
+	}
+	while ((*(s) >= '0' && (*(s) <= '9')))
+	{
+		nbr = nbr * 10 + *s - 48;
+		s++;
+	}
+	sign = sign < 0 ? -1 : 1;
+	return (nbr * sign);
+}
