7f865dac04ab75f9dd98b7851f4f859d41416b33
gchuinard <gautier.chuinard@lilo.org>
2020-10-01 21:28:04 +0000

    fix task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -40,15 +40,3 @@ char	*cap_string(char *str)
 	}
 	return (str);
 }
-
-
-int main(void)
-{
-	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-	char *p;
-
-	p = cap_string(s);
-	printf("%s", p);
-	printf("%s", s);
-	return (0);
-}
