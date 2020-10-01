4bece054b19d41a836d3ee26aa5f00c12759983d
gchuinard <gautier.chuinard@lilo.org>
2020-10-01 21:27:09 +0000

    fix task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,9 +15,11 @@ char	*cap_string(char *str)
 	int	j;
 	bool	is_sep;
 
+	i = 0;
 	is_sep = true;
 	while (str[i])
 	{
+		j = 0;
 		if (is_sep)
 		{
 			if (str[i] >= 'a' && str[i] <= 'z')
@@ -28,7 +30,7 @@ char	*cap_string(char *str)
 		}
 		while (sep[j])
 		{
-			if (str[i] == separators[i])
+			if (str[i] == sep[j])
 			{
 				is_sep = true;
 			}
@@ -37,4 +39,16 @@ char	*cap_string(char *str)
 		i++;
 	}
 	return (str);
+}
+
+
+int main(void)
+{
+	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	char *p;
+
+	p = cap_string(s);
+	printf("%s", p);
+	printf("%s", s);
+	return (0);
 }
