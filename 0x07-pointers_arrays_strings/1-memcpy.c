215f7564d16e8e531213b37c03fe0a7242b3047d
gchuinard <gautier.chuinard@lilo.org>
2020-10-05 10:54:02 +0000

    fix task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,15 +9,15 @@
  *
  * Return: dest (Always).
  */
-
-char	*_memcpy(char *dest, char *src, unsigned int n);
+char	*_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
 	i = 0;
-	while (i++ < n)
+	while (i < n)
 	{
 		dest[i] = src[i];
+		i++;
 	}
 	return (dest);
 }
