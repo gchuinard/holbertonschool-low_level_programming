11df5dc5d0c2a5fed6d8789054dfd42c49e0002e
gchuinard <gautier.chuinard@lilo.org>
2020-10-05 11:23:41 +0000

    fix task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -32,17 +32,3 @@ char	*_strchr(char *s, char c)
 	}
 	return (result);
 }
-
-int main(void)
-{
-	char *s = "hello";
-	char *f;
-
-	f = _strchr(s, 'l');
-
-	if (f != NULL)
-	{
-		printf("%s\n", f);
-	}
-	return (0);
-}
