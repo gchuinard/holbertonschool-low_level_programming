18dd0ed2e2759794f193312b42aededbad75a95a
gchuinard <gautier.chuinard@lilo.org>
2020-10-07 14:29:38 +0000

    add task 7

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -59,18 +59,3 @@ int	is_palindrome(char *s)
 	}
 	return (_check_palindrome(s, --len, 0));
 }
-
-int main(void)
-{
-	int r;
-
-	r = is_palindrome("level");
-	printf("%d\n", r);
-	r = is_palindrome("redder");
-	printf("%d\n", r);
-	r = is_palindrome("holberton");
-	printf("%d\n", r);
-	r = is_palindrome("step on no pets");
-	printf("%d\n", r);
-	return (0);
-}
