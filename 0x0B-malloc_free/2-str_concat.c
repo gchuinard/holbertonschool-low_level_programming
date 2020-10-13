67b713f338808a16c2e02cba7fabd23cb3a2461d
gchuinard <gautier.chuinard@lilo.org>
2020-10-13 10:15:59 +0000

    add task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,69 @@
+#include "holberton.h"
+
+/**
+ * _strlen_cat - return the size of a string or 1 if string == NULL.
+ *
+ * @str: the string
+ *
+ * Return: the size of a string or 1.
+ */
+
+size_t	_strlen_cat(char *str)
+{
+	size_t	len;
+
+	if (str == NULL)
+	{
+		return (1);
+	}
+	len = 0;
+	while (str[len])
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * str_concat - concatenates two strings.
+ *
+ * @s1: the first string.
+ * @s2: the second string.
+ *
+ * Return: the concatenates strings or NULL.
+ */
+
+char	*str_concat(char *s1, char *s2)
+{
+	char		*dest;
+	size_t		len;
+	unsigned int	i;
+	unsigned int	j;
+
+	dest = NULL;
+	len = _strlen_cat(s1) + _strlen_cat(s2) - 1;
+	if ((dest = (char *)malloc(sizeof(char) * len)))
+	{
+		i = 0;
+		if (s1 != NULL)
+		{
+			while (s1[i])
+			{
+				dest[i] = s1[i];
+				i++;
+			}
+		}
+		if (s2 != NULL)
+		{
+			j = 0;
+			while (s2[j])
+			{
+				dest[i] = s2[j];
+				i++;
+				j++;
+			}
+		}
+		dest[i] = '\0';
+	}
+	return (dest);
+}
