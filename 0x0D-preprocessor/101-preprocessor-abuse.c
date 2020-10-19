7af3fa80d5d478c3374883054d8e5c10cbfd47cf
gchuinard <gautier.chuinard@lilo.org>
2020-10-19 16:42:07 +0000

    fix task 6

--- 0x0D-preprocessor/101-preprocessor-abuse.c
+++ 0x0D-preprocessor/101-preprocessor-abuse.c
@@ -1,11 +1,2 @@
-#include <stdio.h>
-
-#define MSG "Hello, Holberton"
-
-/**
- * main - prints a message.
- *
- * Return: Always 0.
- */
-
-main() {printf("%s\n", MSG); }
+#define MSG "Hello, Holberton"/** * main - prints MSG. * * Return: Always 0. */
+main() {puts(MSG); }
