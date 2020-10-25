f6ee209103f510852b1d3750a652eb7654314ea6
gchuinard <gautier.chuinard@lilo.org>
2020-10-25 12:43:46 +0000

    add task 100

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,16 @@
+section .data
+	string db	`Hello, Holberton\n`
+	string_len equ	$-string
+
+section .text
+	global main
+main:
+	mov ebx, 1
+	mov eax, 4
+	mov ecx, string
+	mov edx, string_len
+	int 0x80
+
+	mov ebx, 0
+	mov eax, 1
+	int 0x80
