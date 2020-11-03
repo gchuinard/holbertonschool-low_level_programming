72525b38a059f1a5e33684c6c2f9bc6ac20691d6
gchuinard <gautier.chuinard@lilo.org>
2020-11-03 14:44:36 +0000

    fix task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,11 +12,14 @@ void	free_listint2(listint_t **head)
 {
 	listint_t	*next_node;
 
-	while (*head != NULL)
+	if (head != NULL)
 	{
-		next_node = (*head)->next;
-		free(*head);
-		*head = next_node;
+		while (*head != NULL)
+		{
+			next_node = (*head)->next;
+			free(*head);
+			*head = next_node;
+		}
+		*head = NULL;
 	}
-	*head = NULL;
 }
