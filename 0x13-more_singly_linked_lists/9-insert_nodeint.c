517256735b2e3d64f112b63a5d1696891c78240f
gchuinard <gautier.chuinard@lilo.org>
2020-11-03 14:49:22 +0000

    fix task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t	*insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t	*new_node;
 	listint_t	*tmp_node;
 
-	if (*head != NULL)
+	if (head != NULL && *head != NULL)
 	{
 		new_node = (listint_t *)malloc(sizeof(listint_t));
 		if (new_node != NULL)
