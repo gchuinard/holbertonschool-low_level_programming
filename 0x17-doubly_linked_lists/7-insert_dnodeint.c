d1ed5625c105d5939586c630af1d3ae9a3b9af58
gchuinard <gautier.chuinard@lilo.org>
2021-02-03 23:49:29 +0000

    add task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,7 +2,9 @@
 
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position.
- * @head: the list.
+ * @h: the list.
+ * @idx: the index where we want add the node.
+ * @n: the value of n.
  *
  * Return: the address of the new element, or NULL if it failed.
  */
@@ -13,37 +15,35 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t	*node;
 
 	node = NULL;
-	if (*h != NULL)
+	tmp = *h;
+	if (idx == 0)
 	{
-		tmp = *h;
-		if (idx == 0)
+		node = (dlistint_t *)malloc(sizeof(dlistint_t));
+		if (node == NULL)
+			return (NULL);
+		node->n = n;
+		node->prev = NULL;
+		node->next = *h;
+		if (*h)
+			tmp->prev = node;
+		*h = node;
+		return (*h);
+	}
+	while (tmp != NULL && idx > 0)
+	{
+		if (idx == 1)
 		{
-			node = (dlistint_t *)malloc(sizeof(dlistint_t));
+			node = malloc(sizeof(dlistint_t));
 			if (node == NULL)
 				return (NULL);
 			node->n = n;
-			node->prev = NULL;
-			node->next = *h;
-			tmp->prev = node;
-			*h = node;
-			return (*h);
+			node->prev = tmp;
+			node->next = tmp->next;
+			tmp->next = node;
+			break;
 		}
-			while (tmp != NULL)
-			{
-				if (idx == 1)
-				{
-					node = malloc(sizeof(dlistint_t));
-					if (node == NULL)
-						return (NULL);
-					node->n = n;
-					node->prev = tmp;
-					node->next = tmp->next;
-					tmp->next = node;
-					break;
-				}
-				tmp = tmp->next;
-				idx--;
-		}
+		tmp = tmp->next;
+		idx--;
 	}
 	return (node);
 }
